1d607582c94f2d934b373868f072de5145f2385f
Arttu Ikonen <arttu.ikonen@outlook.com>
2025-05-27 00:00:46 +0300

    Refactor Project and Projects components for improved structure and readability

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,11 +1,11 @@
 import Tag from "./Tag";
 
-const Project = ({ title, imgage, tags }) => {
+const Project = ({ title, image, tags }) => {
   return (
-    <div className="flex aspect-[1.15/1] h-[292.65px] w-[340px] flex-col">
-      <div className="aspect-4/3 h-[255px] w-[340px] bg-gray-50" />
-      <div className="flex h-full w-full flex-row items-center justify-center gap-[4px]">
-        <p className="w-full">{title}</p>
+    <div className="h-min max-w-[calc(25%-16px)] flex-shrink flex-grow flex-col">
+      <div className="aspect-4/3 flex-shrink flex-grow bg-gray-50" />
+      <div className="flex h-full w-full flex-row items-center justify-center gap-[4px] pt-[8px]">
+        <p className="w-full text-nowrap">{title}</p>
         {tags.map((t) => (
           <Tag key={t} text={t} />
         ))}
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,11 +2,19 @@ import Project from "./Project";
 
 const Projects = () => {
   return (
-    <div className="flex h-[600px] w-full flex-wrap gap-[16px]">
+    <div className="flex h-min w-full flex-wrap gap-[16px]">
       <p>projects</p>
       <div className="flex w-full gap-[16px]">
         <Project
           title={"Malvah"}
+          tags={["Next.js", "React Three Fiber", "React"]}
+        />
+        <Project
+          title={"Studio Freight"}
+          tags={["Next.js", "React Three Fiber", "React"]}
+        />
+        <Project
+          title={"Studio Freight"}
           tags={["Next.js", "React Three Fiber", "React"]}
         />
         <Project
