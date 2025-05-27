f928e0e886510260583f8a3ea0c92af76167e48e
Arttu Ikonen <arttu.ikonen@outlook.com>
2025-05-27 23:27:16 +0300

    Made project cards more accessible.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,22 +5,20 @@ const Project = ({ title, image, tags, link }) => {
   const [active, setActive] = useState(false);
   return (
     <div className="w-full">
-      <div
+      <a
         className="relative w-full"
         onMouseEnter={() => setActive(true)}
         onMouseLeave={() => setActive(false)}
+        href={link}
+        target="_blank"
       >
         <img className="aspect-[4/3] w-full bg-gray-50" src={image} />
         {active && (
-          <a
-            className="absolute inset-0 flex cursor-pointer items-center justify-center bg-black/50"
-            href={link}
-            target="_blank"
-          >
+          <p className="absolute inset-0 flex cursor-pointer items-center justify-center bg-black/50">
             See more
-          </a>
+          </p>
         )}
-      </div>
+      </a>
       <div className="flex flex-row items-center justify-center gap-1 pt-4">
         <p className="w-full text-nowrap">{title}</p>
         {tags.map((t) => (
