2dab56b3aed5d0748545ae9406786c5caa95f44f
Arttu Ikonen <arttu.ikonen@outlook.com>
2025-06-02 23:48:50 +0300

    Added basic 3D model to header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Scene } from "three";
+
 const Header = () => {
+  const { nodes, materials } = useGLTF("/ball.glb");
   return (
     <header className="flex h-fit w-full flex-col gap-4">
       <nav className="flex h-fit w-full justify-between">
-        <div className="flex flex-row items-center gap-2 w-full justify-start">
+        <div className="flex w-full flex-row items-center justify-start gap-2">
           <h1>ARTTU IKONEN</h1>
           <h2>SOFTWARE DEVELOPER</h2>
         </div>
-        <div className="flex flex-row items-center gap-2  w-full justify-center">
+        <div className="flex w-full flex-row items-center justify-center gap-2">
           <a href="https://github.com/aaikon" target="_blank">
             GITHUB
           </a>
@@ -17,12 +22,26 @@ const Header = () => {
             LINKED IN
           </a>
         </div>
-        <div className="flex flex-row items-center gap-2  w-full justify-end">
+        <div className="flex w-full flex-row items-center justify-end gap-2">
           <h1>TURKU</h1>
           <div className="size-2 rounded-full bg-white" />
         </div>
       </nav>
-      <div className="w-full h-[500px] bg-neutral-500" />
+      <div className="h-[500px] w-full">
+        <Canvas>
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[2, 2, 2]} intensity={1} />
+          <mesh geometry={nodes.ball.geometry} scale={0.02}>
+            <meshPhysicalMaterial
+              map={materials.material.map}
+              roughnessMap={materials.material.roughnessMap}
+              metalnessMap={materials.material.metalnessMap}
+              normalMap={materials.material.normalMap}
+            />
+          </mesh>
+          <OrbitControls autoRotate autoRotateSpeed={1} />
+        </Canvas>
+      </div>
     </header>
   );
 };
